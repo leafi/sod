dee76a4966138f6162b58435a17972571136c1e5
leafi <leaf@leaf.sx>
2015-02-18 23:12:19 +0000

    fuck me but apple makes things hard

--- sod.Mac/EditorUI/RenderBoxMacOGLHandler.cs
+++ sod.Mac/EditorUI/RenderBoxMacOGLHandler.cs
@@ -4,6 +4,7 @@ using MonoMac.AppKit;
 using System;
 using MonoMac.OpenGL;
 using Eto.Drawing;
+using OpenTK.Graphics;
 
 namespace sod.Mac
 {
@@ -14,6 +15,8 @@ namespace sod.Mac
 
         public GLView()
         {
+            /*Tao.Glfw.glfwOpenWindow(640, 480, 8, 8, 8, 8, 16, 0, Tao.Glfw.GLFW_WINDOW);
+OpenTK.Graphics.GraphicsContext.CreateDummyContext();*/
         }
 
         static void DrawTriangle ()
@@ -28,16 +31,101 @@ namespace sod.Mac
             GL.End ();
         }
 
+        bool first = true;
+
+
+
         public override void DrawRect(System.Drawing.RectangleF dirtyRect)
         {
             this.OpenGLContext.MakeCurrentContext ();
 
+            if (first)
+            {
+                first = false;
+
+
+                /*float points[] = {
+   0.0f,  0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f,
+  -0.5f, -0.5f,  0.0f
+};
+GLuint vbo = 0;
+glGenBuffers (1, &vbo);
+glBindBuffer (GL_ARRAY_BUFFER, vbo);
+glBufferData (GL_ARRAY_BUFFER, 9 * sizeof (float), points, GL_STATIC_DRAW);
+GLuint vao = 0;
+glGenVertexArrays (1, &vao);
+glBindVertexArray (vao);
+glEnableVertexAttribArray (0);
+glBindBuffer (GL_ARRAY_BUFFER, vbo);
+glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+const char* vertex_shader =
+"#version 400\n"
+"in vec3 vp;"
+"void main () {"
+"  gl_Position = vec4 (vp, 1.0);"
+"}";
+// version 3.3/4.1 yosemite, else 3.2
+
+const char* fragment_shader =
+"#version 400\n"
+"out vec4 frag_colour;"
+"void main () {"
+"  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+"}";
+GLuint vs = glCreateShader (GL_VERTEX_SHADER);
+glShaderSource (vs, 1, &vertex_shader, NULL);
+glCompileShader (vs);
+GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
+glShaderSource (fs, 1, &fragment_shader, NULL);
+glCompileShader (fs);
+GLuint shader_programme = glCreateProgram ();
+glAttachShader (shader_programme, fs);
+glAttachShader (shader_programme, vs);
+glLinkProgram (shader_programme);
+
+while (!glfwWindowShouldClose (window)) {
+  // wipe the drawing surface clear
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glUseProgram (shader_programme);
+  glBindVertexArray (vao);
+  // draw points 0-3 from the currently bound VAO with current in-use shader
+  glDrawArrays (GL_TRIANGLES, 0, 3);
+  // update other events like input handling 
+  glfwPollEvents ();
+  // put the stuff we've been drawing onto the display
+  glfwSwapBuffers (window);
+}
+*/
+
+
+            }
+
             //GL.ClearColor(0.5f, 0f, 0f, 1f);
 
-            GL.ClearColor (0, 0, 0, 0);
+            //GL.ClearColor (0, 0, 0, 0);
+            //GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Disable(EnableCap.CullFace);
+            GL.Viewport(0, 0, 400, 300);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, 400, 300, 0, -1, 1);
+            GL.Disable(EnableCap.DepthTest);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.ClearColor(0f, 1f, 1f, 1f);
             GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Begin(BeginMode.Triangles);
+            GL.Color3(127, 127, 127);
+            GL.Vertex2(100, 100);
+            GL.Vertex2(100, 200);
+            GL.Vertex2(0, 200);
+            GL.Vertex2(100, 100);
+            GL.Vertex2(100, 200);
+            GL.Vertex2(200, 200);
+            GL.Finish();
 
-            DrawTriangle ();
+            //DrawTriangle ();
 
             GL.Flush ();
         }
@@ -57,19 +145,20 @@ namespace sod.Mac
         {
             return new NSOpenGLPixelFormatAttribute[] {
                 NSOpenGLPixelFormatAttribute.Accelerated,
-                NSOpenGLPixelFormatAttribute.OpenGLProfile, (NSOpenGLPixelFormatAttribute)0x3200,
+                //NSOpenGLPixelFormatAttribute.OpenGLProfile, (NSOpenGLPixelFormatAttribute)0x3200,
                 NSOpenGLPixelFormatAttribute.ColorSize, (NSOpenGLPixelFormatAttribute)24,
-                NSOpenGLPixelFormatAttribute.AlphaSize, (NSOpenGLPixelFormatAttribute)8
+                NSOpenGLPixelFormatAttribute.AlphaSize, (NSOpenGLPixelFormatAttribute)8,
             };
         }
 
         public RenderBoxMacOGLHandler()
         {
-            Control = new GLView { Handler = this, PixelFormat = new NSOpenGLPixelFormat(ChoosePixelFormat()) };
-            //Eto.Mac.Forms.mac
-            //new Eto.Drawing.RectangleFConverter(
+            var glv = new GLView { Handler = this, PixelFormat = new NSOpenGLPixelFormat(ChoosePixelFormat()) };
+            Control = glv;
 
-            //this.Control = new GLView(
+            //glv.OpenGLContext.MakeCurrentContext();
+            //new GraphicsContext(
+
         }
 
         public override Eto.Drawing.SizeF GetPreferredSize(Eto.Drawing.SizeF availableSize)
