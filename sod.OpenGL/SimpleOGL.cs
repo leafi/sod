6c481e6ceab1872c042d46219e4ad118d5f0446c
leafi <leaf@leaf.sx>
2015-02-22 19:47:42 +0000

    o opentk gamewindow

--- sod.OpenGL/SimpleOGL.cs
+++ sod.OpenGL/SimpleOGL.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK.Graphics.OpenGL;
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
 using System;
 
 namespace sod.OpenGL
@@ -9,18 +10,52 @@ namespace sod.OpenGL
         {
         }
 
+        public event Action<int, int, int, int> Resize = null;
+
+        public void OnResize(int x, int y, int w, int h)
+        {
+            GL.Viewport(x, y, w, h);
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4f, (float)w / (float)h, 1f, 1000f);
+            Matrix4 ortho = Matrix4.CreateOrthographic(w, h, 1f, 1000f);
+            GL.MatrixMode(MatrixMode.Projection);
+            //GL.LoadMatrix(ref ortho);
+            GL.LoadMatrix(ref perspective);
+
+            // o de fake
+            //GL.MatrixMode(MatrixMode.Modelview);
+            //GL.LoadIdentity();
+
+            if (Resize != null)
+                Resize(x, y, w, h);
+        }
+
+        public void OnLoad()
+        {
+            OnResize(0, 0, 400, 300);
+        }
+
+        public void OnUnload()
+        {
+
+        }
+
         public void Render()
         {
-            GL.Viewport(0, 0, 400, 300);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadIdentity();
-            GL.Ortho(0, 400, 300, 0, -1, 1);
-            GL.Disable(EnableCap.DepthTest);
+            //GL.Viewport(0, 0, 400, 300);
+            //GL.MatrixMode(MatrixMode.Projection);
+            //GL.LoadIdentity();
+            //GL.Ortho(0, 400, 300, 0, -1, 1);
+            //GL.Disable(EnableCap.DepthTest);
+            //GL.MatrixMode(MatrixMode.Modelview);
+            //GL.LoadIdentity();
+
+            Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
             GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadIdentity();
+            GL.LoadMatrix(ref modelview);
+
             GL.ClearColor(0f, 1f, 1f, 1f);
-            GL.Clear(ClearBufferMask.ColorBufferBit);
-            GL.Begin(BeginMode.Triangles);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Begin(PrimitiveType.Triangles);
             GL.Color3(127, 127, 127);
             GL.Vertex2(100, 100);
             GL.Vertex2(100, 200);
@@ -28,7 +63,7 @@ namespace sod.OpenGL
             GL.Vertex2(100, 100);
             GL.Vertex2(100, 200);
             GL.Vertex2(200, 200);
-            GL.Finish();
+            GL.End();
         }
     }
 }
---
+++ sod.RenderTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenTK;
+using sod.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sod.RenderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleOGL ogl = new SimpleOGL();
+
+            using (var gw = new GameWindow())
+            {
+                gw.VSync = VSyncMode.On;
+
+                gw.Load += (sender, ev) => { ogl.OnLoad(); };
+                gw.Unload += (sender, ev) => { ogl.OnUnload(); };
+
+                gw.Resize += (sender, ev) => { ogl.OnResize(gw.ClientRectangle.Left, gw.ClientRectangle.Top, gw.ClientRectangle.Width, gw.ClientRectangle.Height); };
+
+                gw.RenderFrame += (sender, ev) => { ogl.Render(); gw.SwapBuffers(); };
+                gw.UpdateFrame += (sender, ev) => { };
+
+                gw.Run(60.0); // logic up to 60fps & render up to refresh rate. TODO: detect vsync/give option to player?
+            }
+        }
+    }
+}
